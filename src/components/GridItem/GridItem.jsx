4b735c89972029f622c207dca07e5e608c7db35c
Olexandrd <olexandrd@gmail.com>
2024-08-26 16:53:29 +0300

    task2

--- src/components/GridItem/GridItem.jsx
+++ src/components/GridItem/GridItem.jsx
@@ -1,4 +1,18 @@
-import style from './GridItem.module.css';
-export const GridItem = ({ children }) => {
-  return <li className={style.item}>{children}</li>;
+import styles from './GridItem.module.css';
+
+export const GridItem = ({ item }) => {
+  if (!item) {
+    return null;
+  }
+
+  return (
+    <li className={styles.item}>
+      <div
+        className={styles.thumb}
+        style={{ backgroundColor: item.avg_color, borderColor: item.avg_color }}
+      >
+        <img src={item.src.large} alt={item.alt} />
+      </div>
+    </li>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,51 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Button, Form, Grid, PhotosGalleryItem } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [array, setArray] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const onSubmit = text => {
+    setSearch(text);
+  };
+
+  useEffect(() => {
+    if (!search) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    getPhotos(search, page)
+      .then(data => {
+        setArray(prev => [...prev, ...data.photos]);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [search, array, page]);
+
+  const loadMoreHandler = () => {
+    setPage(prev => prev + 1);
+  };
+
   return (
     <>
-      <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={onSubmit} />
+      {
+        <Grid>
+          {array.length > 0 &&
+            array.map(item => <PhotosGalleryItem key={item.id} item={item} />)}
+        </Grid>
+      }
+      {isLoading && <p>Loading...</p>}
+      {array.length > 0 && <Button onClick={loadMoreHandler}>Load more</Button>}
     </>
   );
 };
